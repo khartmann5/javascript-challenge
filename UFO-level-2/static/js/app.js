80aac1aec3c4177e10a9cb110711b39a17cf589e
khartmann5 <hartmann.kt@gmail.com>
2021-03-28 19:20:07 -0700

    adding UFO level 2 files

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,48 @@
+// from data.js
+// Assign the data from `data.js` to a descriptive variable
+var tableData = data;
+
+// Console.log the UFO data from data.js
+// console.log(data);
+
+// Use d3 to update each cell's text with
+// UFO report values (Date, City, State, Country, Shape, Duration, Comments)
+function showtable(tableData){
+    const tbody = d3.select("tbody");
+    tbody.html("");
+    tableData.forEach(UFO => {
+        let row = tbody.append("tr");
+        Object.values(UFO).forEach(value => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });
+};
+
+// write to html
+showtable(tableData);
+
+// Select the button
+// Select the form
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
+
+// Complete the event handler function for the form
+const runEnter = () => {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    var filterDate = tableData.filter(UFO => UFO.datetime === inputValue);
+    console.log(filterDate);
+    let response = {
+        filterDate
+    }
+    if(response.filterDate.length !== 0) {
+        showtable(filterDate);
+    }
+};
+
+// Create event handlers
+button.on("click",runEnter);
+form.on("submit",runEnter);
