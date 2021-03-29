38170a4cbf73f1bea47d20a73dd5e7b17f534721
khartmann5 <hartmann.kt@gmail.com>
2021-03-28 18:48:20 -0700

    saving work on filter button

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,28 +1,43 @@
 // from data.js
 // Assign the data from `data.js` to a descriptive variable
-// Select the button
-// Select the form
-var tableData = data,
-    button = d3.select("#filter-btn"),
-    form = d3.select("#form");
-
-// Get a reference to the table body
-const tbody = d3.select("tbody");
+var tableData = data;
 
 // Console.log the UFO data from data.js
 // console.log(data);
 
-// Step 1: Loop Through `data` and console.log each UFO report object
-data.forEach(UFO => console.log(UFO));
-
 // Use d3 to update each cell's text with
 // UFO report values (Date, City, State, Country, Shape, Duration, Comments)
-data.forEach(UFO => {
-  let row = tbody.append("tr");
-  Object.values(UFO).forEach(value => {
-    // Append a cell to the row for each value
-    // in the weather report object
-    var cell = row.append("td");
-    cell.text(value);
-  });
+function showtable(tableData){
+    const tbody = d3.select("tbody");
+    tbody.html("");
+    tableData.forEach(UFO => {
+        let row = tbody.append("tr");
+        Object.values(UFO).forEach(value => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });
+};
+
+// write to html
+showtable(tableData);
+
+// Complete the event handler function for the form
+var button = d3.select("#filter-btn");
+button.on("click", function(){
+    d3.event.preventDefault();
+    var inputElement = d3.select("datetime"),
+        inputValue = inputElement.property("value");
+    var filterDate = tableData.filter(tableData => tableData.datetime === inputValue);
+
+    tbody.html("");
+
+    let response = {
+        filterDate
+    }
+
+    if(response.filterDate.length !== 0) {
+        showtable(filterDate)
+    }
+        else {tbody.append("tr").append("td").text("No sightings on this date.")}
 });
